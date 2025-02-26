7f94e0b820b969a40a453efd890c7b6b1388fd03
Matt Waldron <mattwaldron@outlook.com>
2025-02-26 17:20:32 -0500

    Refactor API and Weather Hub integration in app settings.
    Updated WeatherEndpoint to a new URL in appsettings.json for consistency. Refactored Program.cs to establish a clearer project reference and enhance encapsulation by isolating external HTTP endpoints.

--- start-with-api/ZorkAppHost/Program.cs
+++ start-with-api/ZorkAppHost/Program.cs
@@ -2,7 +2,11 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 // The distributed application builder is getting a reference to
 // a .net project
-var api = builder.AddProject<Projects.Api>("api");
-var hub = builder.AddProject<Projects.Api>("myweatherhub");
+var myapi = builder.AddProject<Projects.Api>("api");
+
+var hub = builder
+    .AddProject<Projects.MyWeatherHub>("myweatherhub")
+    .WithReference(myapi) // reference the api project from the weather hub project
+    .WithExternalHttpEndpoints();  // hides the API project from the outside world
 
 builder.Build().Run();
